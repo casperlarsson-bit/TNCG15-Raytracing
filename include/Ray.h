2d5189c0a7eb6de0a8104e6b0c39d95f60f23616
Casper Larsson <casper.larsson@live.se>
2022-09-06 16:39:40 +0200

    Added foundation to Ray
    Added base structure to Ray class, and imported glm library for vec etc.

--- include/Ray.h
+++ include/Ray.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "ColorRGB.h"
+#include "glm/glm.hpp"
+
+class Ray {
+public:
+	// Default constructor
+	Ray();
+
+	// Destructor
+	~Ray();
+
+
+private:
+	glm::vec4 startingPoint; // Need to implement vertex/vector?
+	glm::vec4 endPoint;
+	glm::vec3 direction;
+
+	Ray* prevRay;
+	Ray* nextRay;
+	// Surface* startingVertex; // Implement Surface?
+
+	ColorRGB rayColor;
+};
