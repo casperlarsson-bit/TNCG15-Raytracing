9014ddb00f21a8e23b1d4658fd6e8781467b7afa
Casper Larsson <casper.larsson@live.se>
2022-09-08 12:16:42 +0200

    Update Camera.cpp
    Changed method for saving image

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,6 +1,6 @@
 #include "../include/Camera.h"
-
 #include <iostream>
+#include <fstream>
 
 // Default constructor sets resolution to 600x600
 Camera::Camera() : image(SIZE, std::vector<Pixel>(SIZE)) {
@@ -26,19 +26,33 @@ void Camera::renderImage() {
 // Save the image as file (.jpg?)
 void Camera::saveImage() const {
 	// Save image
-	FILE* fp = fopen("Raytracing image.bmp", "wb");
+	/*FILE* fp = fopen("Raytracing image.bmp", "wb");
 
 	for (auto& row : image) {
 		for (auto& pix : row) {
 			// Get pixel
 			unsigned char color[3];
-			color[0] = (unsigned char)pix.getColor().red;
-			color[1] = (unsigned char)pix.getColor().green;
-			color[2] = (unsigned char)pix.getColor().blue;
+			color[0] = (unsigned char)pix.getColor().red*255;
+			color[1] = (unsigned char)pix.getColor().green*255;
+			color[2] = (unsigned char)pix.getColor().blue*255;
 
 			fwrite(color, 1, 3, fp);
 		}
 	}
 
-	fclose(fp);
+	fclose(fp);*/
+
+	std::ofstream ofs("Raytracing.ppm", std::ios::out | std::ios::binary);
+	ofs << "P6\n" << image.size() << " " << image.size() << "\n255\n";
+
+	for (auto& row : image) {
+		for (auto& pix : row) {
+			// Get pixel
+			ofs << (unsigned char)pix.getColor().red * (float)255 <<
+				(unsigned char)pix.getColor().green * (float)255 <<
+				(unsigned char)pix.getColor().blue * (float)255;
+		}
+	}
+
+	ofs.close();
 }
